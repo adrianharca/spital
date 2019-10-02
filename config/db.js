fe6b601bbd3cb721f8bdd3c9b1efde76c9d7f064
kjj <gabriela.vladescu@gmail.com>
2019-10-02 16:33:40 +0300

    first

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+const db = new Sequelize('circles', 'root', 'root', {
+    host: 'localhost',
+    port: '3308',
+    dialect: 'mysql',
+    operatorAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+db.authenticate().then(() => console.log('Db connected'+ db.)).catch(err => console.log('Err: ' + err));
