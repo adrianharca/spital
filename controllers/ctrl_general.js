6008a7f5f487483e380e36f5c18a034427d54f19
kjj <gabriela.vladescu@gmail.com>
2019-12-23 14:49:51 +0200

    xftsrr

--- controllers/ctrl_general.js
+++ controllers/ctrl_general.js
@@ -1,7 +1,7 @@
 exports.checkForStale= function(device,server){
 if(device.modifiedAt.getTime()<server.modifiedAt.getTime())
      {update(device);
-    return Maybe.; 
+    return Maybe; 
     }
     else if(device.modifiedAt.getTime()>server.modifiedAt.getTime())
     return server;// and send for local update
